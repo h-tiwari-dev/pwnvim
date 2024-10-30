d6d260d56e0dafc0b3ec67a5bcfff5077d1e3180
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2024-10-30 21:23:25 +0530

    One more changes

--- pwnvim/mappings.lua
+++ pwnvim/mappings.lua
@@ -373,6 +373,14 @@ M.config = function()
     end)
   end, "Open folder")
 
+  -- Map <leader>u to toggle the UndoTree plugin, allowing undo history visualization
+  M.mapleadernv("u", require('undotree').toggle, "Toggle UndoTree")
+  -- Map <leader>uo to open the UndoTree explicitly
+  M.mapleadernv("uo", require('undotree').open, "Open UndoTree")
+  -- Map <leader>uc to close the UndoTree explicitly
+  M.mapleadernv("uc", require('undotree').close, "Close UndoTree")
+
+
   -- Quickly change indent defaults in a file
   M.mapleadernv("i1", require('pwnvim.options').tabindent, "Tab")
   M.mapleadernv("i2", require('pwnvim.options').twospaceindent, "Two Space")
